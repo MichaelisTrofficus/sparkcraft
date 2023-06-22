1b706b15b846741e89be68d191bbaff62f4657b6
MichaelisTrofficus <miguel.otero.pedrido.1993@gmail.com>
2023-06-23 00:34:57 +0200

    Refactor project structure

--- src/sparkcraft/joins.py
+++ src/sparkcraft/joins.py
@@ -3,7 +3,7 @@ from pyspark.sql import DataFrame
 from pyspark.sql.types import IntegerType
 
 
-def add_salt_column(df: DataFrame, skew_factor: int):
+def add_salt_column(df: DataFrame, skew_factor: int) -> DataFrame:
     """
     Adds a salt column to a DataFrame. We will be using this salt column when we are trying to perform
     join, groupBy, etc. operations into a skewed DataFrame. The idea is to add a random column and use
@@ -16,3 +16,20 @@ def add_salt_column(df: DataFrame, skew_factor: int):
     :return: The original DataFrame with a `salt_id` column.
     """
     return df.withColumn("salt_id", (sf.rand() * skew_factor).cast(IntegerType()))
+
+
+def optimal_cross_join(df_bc: DataFrame, df: DataFrame) -> DataFrame:
+    """
+    A simple trick to solve the problem we have with CrossJoins between two dataframes,
+    when the resulting partitions will be a multiplication of the initial partitions (e.g. if
+    we make the cross join between `df1` and `df2` both with 100 partitions, then the resulting
+    partitions will be 10_000).
+
+    Args:
+        df_bc: The DataFrame to be broadcasted
+        df: The DataFrame
+
+    Returns:
+        The cross joined DataFrame
+    """
+    return sf.broadcast(df_bc).crossJoin(df)
